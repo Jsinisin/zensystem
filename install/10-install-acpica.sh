86d18ced1e15144dd6540d61b509c10b41582541
Jsinisin <161667580+Jsinisin@users.noreply.github.com>
2024-06-01 20:01:12 -0600

    Update and rename 10-install-extras to 10-install-acpica.sh

---
+++ install/10-install-acpica.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd /home/user/.sources/acpica
+
+wget https://drive.proton.me/urls/NQNZXH9MNW#rPwwrwiWON1o
+
+wget https://drive.proton.me/urls/7W4H5J77Y4#YgJrYFhbQVCC
+
+iasl -tc ssdt_csc3551.dsl
+
+sudo cp -f ssdt_csc3551.aml /boot
+
+sudo cp -f 01_acpi.txt /etc/grub.d
+
+sudo chmod +x /etc/grub.d/01_acpi.txt
+
+
