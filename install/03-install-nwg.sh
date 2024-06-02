53a6e0fa8fb64a3ddb59031f7d14a17407f7ba28
Jsinisin <161667580+Jsinisin@users.noreply.github.com>
2024-06-01 19:44:51 -0600

    Update 03-install-nwg.sh

--- install/03-install-nwg.sh
+++ install/03-install-nwg.sh
@@ -4,7 +4,8 @@ cd /home/user/.sources
 
 wget https://github.com/nwg-piotr/nwg-look/archive/refs/tags/v0.2.6.zip
 unzip v0.2.6.zip
+
 cd nwg-look-0.2.6
 
-make build
+sudo make build
 sudo make install
