46294f28bb4aa52bdd043fbcde7554c7cc30ba5b
Jsinisin <161667580+Jsinisin@users.noreply.github.com>
2024-06-01 20:03:37 -0600

    Create 11-install-extras.sh

---
+++ install/11-install-extras.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd /home/user/.sources
+
+wget https://drive.proton.me/urls/B4S3PMJ180#FsjzH24H0YnQ
+
+wget https://drive.proton.me/urls/XCBT5DY9JR#ZA0YXOhTwpl9
+
+wget https://drive.proton.me/urls/2GC5KX0SPW#vdXDT6une1zP
+
+wget https://github.com/standardnotes/desktop/releases/download/v3.22.11/standard-notes-3.22.11-linux-amd64.deb
+
+wget https://drive.proton.me/urls/7KMNC1BMX0#LSPjecIGdpN0
