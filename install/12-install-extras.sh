4c9c580de1cc259ad9d1452626f6ebef6f8805e9
Jsinisin <161667580+Jsinisin@users.noreply.github.com>
2024-06-01 20:13:49 -0600

    Update 12-install-extras.sh

--- install/12-install-extras.sh
+++ install/12-install-extras.sh
@@ -11,3 +11,9 @@ wget https://drive.proton.me/urls/2GC5KX0SPW#vdXDT6une1zP
 wget https://github.com/standardnotes/desktop/releases/download/v3.22.11/standard-notes-3.22.11-linux-amd64.deb
 
 wget https://drive.proton.me/urls/7KMNC1BMX0#LSPjecIGdpN0
+
+wget https://drive.proton.me/urls/29C7Y05XVR#Im5tCaj4MWd7
+
+sudo apt install /home/user/.sources/microsoft-edge-stable_125.0.2535.79-1_amd64.deb
+
+sudo apt install /home/user/.sources/standard-notes-3.22.11-linux-amd64.deb
